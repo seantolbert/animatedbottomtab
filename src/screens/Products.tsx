4ee098bcc719dd71645ea5dbee61fde1ac7c5457
Sean Tolbert <sean@Seans-MacBook-Pro.local>
2023-10-22 14:29:41 -0400

    adds dependencies ran pod install in ios dir and adds initial screens

---
+++ src/screens/Products.tsx
@@ -0,0 +1,20 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const Products = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Products</Text>
+    </View>
+  );
+};
+
+export default Products;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
